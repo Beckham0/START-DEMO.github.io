6b78b7a66858c32c372b82d69c379dca16e86fdb
phone2644 <nicklove115533@gmail.com>
2025-02-01 01:36:03 +0700

    update image slide

--- script.js
+++ script.js
@@ -16,24 +16,23 @@ const swiper = new Swiper('.slider-wrapper', {
   });
 
 
-  const tabs = document.querySelectorAll('.elementor-tab-title');
-  const contents = document.querySelectorAll('.elementor-tab-content');
-
-  tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-      const tabId = tab.dataset.tab;
-      const content = document.querySelector(`.elementor-tab-content[data-tab="${tabId}"]`);
-
-      if (tab.classList.contains('active')) {
-        tab.classList.remove('active');
-        content.classList.remove('active');
-      } else {
+  document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.elementor-tab-title');
+    const contents = document.querySelectorAll('.elementor-tab-content');
+  
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        const tabId = tab.dataset.tab;
+  
         tabs.forEach(t => t.classList.remove('active'));
         contents.forEach(c => c.classList.remove('active'));
-
+  
         tab.classList.add('active');
-        content.classList.add('active');
-      }
+        contents.forEach(content => {
+          if (content.dataset.tab === tabId) {
+            content.classList.add('active');
+          }
+        });
+      });
     });
   });
-  
