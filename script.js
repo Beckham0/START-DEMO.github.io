b5612f8d98c3492ad72bec8635e191684cd62f64
phone2644 <nicklove115533@gmail.com>
2025-01-31 23:32:22 +0700

    update content-Our customer

--- script.js
+++ script.js
@@ -14,4 +14,26 @@ const swiper = new Swiper('.slider-wrapper', {
   
   
   });
+
+
+  const tabs = document.querySelectorAll('.elementor-tab-title');
+  const contents = document.querySelectorAll('.elementor-tab-content');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      const tabId = tab.dataset.tab;
+      const content = document.querySelector(`.elementor-tab-content[data-tab="${tabId}"]`);
+
+      if (tab.classList.contains('active')) {
+        tab.classList.remove('active');
+        content.classList.remove('active');
+      } else {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(c => c.classList.remove('active'));
+
+        tab.classList.add('active');
+        content.classList.add('active');
+      }
+    });
+  });
   
