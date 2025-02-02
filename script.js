50de02fec64b5a13e400830478540c02a495991c
phone2644 <nicklove115533@gmail.com>
2025-02-03 01:44:52 +0700

    update chaty

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ const swiper = new Swiper('.slider-wrapper', {
       });
     });
   });
+
+  const chaty = document.getElementById('chaty')
+  const cancel = document.getElementById('cancel')
+  const line = document.getElementById('line')
+  const call = document.getElementById('call')
+
+  chaty.addEventListener('click', () =>{
+
+    chaty.style.opacity = 0; 
+    cancel.style.opacity = 1; 
+    call.style.opacity = 1; 
+    line.style.opacity = 1; 
+
+    cancel.style.zIndex = 2; 
+    chaty.style.zIndex = 1; 
+    // call.style.zIndex = 2; 
+    // line.style.zIndex = 2; 
+
+    cancel.style.transform = "rotate(0deg)"; 
+    chaty.style.transform = "rotate(360deg)"; 
+    
+    call.style.bottom= "165px"; 
+    line.style.bottom= "105px";  
+    
+  });
+  cancel.addEventListener('click', () =>{
+
+    chaty.style.opacity = 1; 
+    cancel.style.opacity = 0; 
+    call.style.opacity = 0.3; 
+    line.style.opacity = 0.3;
+
+    cancel.style.zIndex = 1; 
+    chaty.style.zIndex = 2; 
+    
+    chaty.style.transform = "rotate(0deg)"; 
+    cancel.style.transform = "rotate(-360deg)"; 
+
+    call.style.bottom= "46px"; 
+    line.style.bottom= "46px"; 
+  });
