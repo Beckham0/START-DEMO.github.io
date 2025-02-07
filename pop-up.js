6fc4102689165698c92d65565abc3e4322e9c083
คุณากร ทำดี <kingmanstar55@gmail.com>
2025-02-07 20:18:32 +0700

    ยกโป้

--- pop-up.js
+++ pop-up.js
@@ -13,3 +13,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     observer.observe(popUp);
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up1");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up2");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
