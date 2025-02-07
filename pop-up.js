3aaaf65ecf75291244b42a90e3a575eb098581bc
คุณากร ทำดี <kingmanstar55@gmail.com>
2025-02-07 20:59:50 +0700

    ดินสอสี

--- pop-up.js
+++ pop-up.js
@@ -46,3 +46,130 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(popUp);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up3");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up4");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up5");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up6");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up7");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up8");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up9");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popUp = document.querySelector(".pop-up10");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                popUp.classList.add("show");
+            } else {
+                popUp.classList.remove("show");
+            }
+        });
+    });
+
+    observer.observe(popUp);
+});
